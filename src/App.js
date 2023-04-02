00caf0bf32c699cecc128dd808e7d94770daed7e
kristianrybar <108004841+kristianrybar@users.noreply.github.com>
2023-04-02 23:25:53 +0200

    not needed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './plugins/app/navbar/navbar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-orange-700 bg-cyan-200">
-          Edit <code>src/App.js</code> and save to reload. test123
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
