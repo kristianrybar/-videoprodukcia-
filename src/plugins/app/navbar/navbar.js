db147ef7fa6768caac7db73a8a9ddf2b60e2259d
kristianrybar <108004841+kristianrybar@users.noreply.github.com>
2023-04-02 23:26:14 +0200

    navbar

---
+++ src/plugins/app/navbar/navbar.js
@@ -0,0 +1,29 @@
+import logo from './assets/logo-dark.svg'
+
+const navbar = () => {
+  return (
+    <header className="bg-[#231f20] fixed inset-x-0 top-0 text-white py-[20px]">
+        <div className="flex items-center justify-center px-[15px]">
+            <a className='block w-[180px] mr-[50px]' href="#">
+                <img src={logo} alt="logo" />
+            </a>
+            <ul className='flex items-center relative top-[4px] text-base text-[#FAFAFA]'>
+                <a className='p-[3px] mr-[30px] hover:text-[red] transition' href="#">
+                    <li>HOME</li>    
+                </a>
+                <a className='p-[3px] mr-[30px] hover:text-[red] transition' href="#">
+                    <li>CALENDAR</li>    
+                </a>
+                <a className='p-[3px] mr-[30px] hover:text-[red] transition' href="#">
+                    <li>DATA</li>    
+                </a>
+                <a className='p-[3px] hover:text-[red] transition' href="#">
+                    <li>STUDIO</li>    
+                </a>
+            </ul>
+        </div>
+    </header>
+  )
+}
+
+export default navbar
