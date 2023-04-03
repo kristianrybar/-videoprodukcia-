2d6493f233f7dd18f4d58e091eff5b8885382794
kristianrybar <108004841+kristianrybar@users.noreply.github.com>
2023-04-03 22:04:16 +0200

    statusbar styling

--- src/plugins/app/statusbar/statusbar.js
+++ src/plugins/app/statusbar/statusbar.js
@@ -1,6 +1,19 @@
 const statusbar = () => {
   return (
-    <div>statusbar</div>
+    <div className="fixed bottom-0 inset-x-0">
+      <div className="flex items-center justify-between bg-[#231f20] py-[5px] px-[15px] text-white">
+        <p className="text-[#cfcfcf]">
+          <span>
+            <code>status: fulfilled, result: connected</code>
+          </span>         
+        </p>
+        <button className="px-2 bg-red-700 rounded-sm">
+          <span className="relative bottom-[1px]">
+            connect
+          </span>
+        </button>
+      </div>
+    </div>
   )
 }
 
