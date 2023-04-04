bcaecc049232601eb967814b68582c75a5088a3c
kristianrybar <108004841+kristianrybar@users.noreply.github.com>
2023-04-04 22:11:01 +0200

    added z-index

--- src/plugins/app/navbar/navbar.js
+++ src/plugins/app/navbar/navbar.js
@@ -2,7 +2,7 @@ import logo from './assets/logo-dark.svg'
 
 const navbar = () => {
   return (
-    <header className="bg-[#231f20] fixed inset-x-0 top-0 text-white py-[20px]">
+    <header className="bg-[#231f20] fixed inset-x-0 top-0 text-white py-[20px] z-[999]">
         <div className="flex items-center justify-start px-[15px]">
             <a className='block w-[180px] mr-[50px] ml-[3%]' href="#">
                 <img src={logo} alt="logo" />
