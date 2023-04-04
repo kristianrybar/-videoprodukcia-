ce258a6d7ebc1505510b7fee09ac4047dcf892c5
kristianrybar <108004841+kristianrybar@users.noreply.github.com>
2023-04-04 22:11:16 +0200

    added z-index

--- src/plugins/app/statusbar/statusbar.js
+++ src/plugins/app/statusbar/statusbar.js
@@ -1,6 +1,6 @@
 const statusbar = () => {
   return (
-    <div className="fixed bottom-0 inset-x-0">
+    <div className="fixed bottom-0 inset-x-0 z-[999]">
       <div className="flex items-center justify-between bg-[#231f20] py-[5px] px-[15px] text-white">
         <p className="text-[#cfcfcf]">
           <span>
