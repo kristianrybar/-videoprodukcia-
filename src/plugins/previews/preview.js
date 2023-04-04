ef681b62c91fb875d1e19274d7e1ced62cb12c7d
kristianrybar <108004841+kristianrybar@users.noreply.github.com>
2023-04-04 22:12:41 +0200

    styling preview component

--- src/plugins/previews/preview.js
+++ src/plugins/previews/preview.js
@@ -1,6 +1,17 @@
 const preview = () => {
   return (
-    <div>preview</div>
+    <div className="flex flex-col h-full">
+
+      <div className="bg-tranparent py-3">
+        <h1 className="text-center">
+          PREVIEW
+        </h1>
+      </div>
+
+      <div className="bg-[#0f0f0f] h-full">
+      </div>
+
+    </div>
   )
 }
 
