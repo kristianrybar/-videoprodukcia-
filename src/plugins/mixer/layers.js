4d45cb8cf2c8a7807df7ebcc90b893a18d204058
kristianrybar <108004841+kristianrybar@users.noreply.github.com>
2023-04-04 22:12:04 +0200

    styling layers component

--- src/plugins/mixer/layers.js
+++ src/plugins/mixer/layers.js
@@ -1,6 +1,58 @@
 const layers = () => {
   return (
-    <div>layers</div>
+    <div className="px-2">
+
+      <div className="bg-tranparent py-3">
+        <h1 className="text-center">
+          LAYERS
+        </h1>
+      </div>
+
+
+      <div className="bg-[black] py-[11px] mb-[1px]">
+        <h3 className="text-[12px] text-center relative top-[-10px]">
+          LAYER1
+        </h3> 
+      </div>
+
+      <div className="bg-[black] py-[11px] mb-[1px]">
+        <h3 className="text-[12px] text-center relative top-[-10px]">
+          LAYER1
+        </h3> 
+      </div>
+
+      <div className="bg-[black] py-[11px] mb-[1px]">
+        <h3 className="text-[12px] text-center relative top-[-10px]">
+          LAYER1
+        </h3> 
+      </div>
+
+      <div className="bg-[black] py-[11px] mb-[1px]">
+        <h3 className="text-[12px] text-center relative top-[-10px]">
+          LAYER1
+        </h3> 
+      </div>
+
+      <div className="bg-[black] py-[11px] mb-[1px]">
+        <h3 className="text-[12px] text-center relative top-[-10px]">
+          LAYER1
+        </h3> 
+      </div>
+
+      <div className="flex items-center justify-around text-[14px] mt-[3px]">
+        <div className="bg-[#231f20] w-full py-3 mr-[2px] cursor-pointer">
+          <h3 className="text-center">
+            ADD
+          </h3>
+        </div>
+        <div className="bg-[#231f20] w-full py-3 ml-[2px] cursor-pointer">
+          <h3 className="text-center">
+            CLEAR
+          </h3>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
