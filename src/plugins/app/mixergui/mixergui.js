68fc133859c2ee3e94eec7ad40964b63b913116d
kristianrybar <108004841+kristianrybar@users.noreply.github.com>
2023-04-04 22:10:41 +0200

    added mixerGUI components

--- src/plugins/app/mixergui/mixergui.js
+++ src/plugins/app/mixergui/mixergui.js
@@ -1,7 +1,24 @@
+import Layers from '../../mixer/layers'
+import Preview from '../../previews/preview'
+import Modes from '../../mixer/modes'
+
+
 const mixergui = () => {
   return (
-    <div className="relative top-[100px]">
-      mixergui
+    <div className="relative top-[70px] h-[100vh] bg-[#303030] text-[#a6a6a6]">
+      <div className="grid grid-cols-12 w-[50%]">
+        <div className='col-span-3'>
+          <Layers />  
+        </div>
+        <div className='col-span-6'>
+          <Preview />
+        </div>
+        <div className='col-span-3'>
+          <Modes />  
+        </div>
+        
+          
+      </div>
     </div>
   )
 }
