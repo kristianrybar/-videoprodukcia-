70ffb58503b16af060750f71e48fb7ddce207e4a
kristianrybar <108004841+kristianrybar@users.noreply.github.com>
2023-04-07 20:06:34 +0200

    cam component

---
+++ src/features/mix_cams/_layout/cam.js
@@ -0,0 +1,42 @@
+const cams = () => {
+  return (
+    <div className="flex flex-col px-2">
+
+        <div>
+            <div className="flex items-center justify-between bg-tranparent py-3">
+                <h1 className="ml-[5px] text-center font-medium relative top-[10px]">
+                    CAM 1
+                </h1>
+                <button className="px-2 bg-red-700 rounded-sm relative top-[7px]">
+                    <span className="relative bottom-[1px]">
+                        connect
+                    </span>
+                </button>
+            </div>
+
+            <div className="min-h-[150px] bg-[#0f0f0f] h-full">
+            </div>
+        </div>
+
+        <div>
+            <div className="flex items-center justify-between bg-tranparent py-3">
+                <h1 className="ml-[5px] text-center font-medium relative top-[10px]">
+                    CAM 2
+                </h1>
+                <button className="px-2 bg-red-700 rounded-sm relative top-[7px]">
+                    <span className="relative bottom-[1px]">
+                        connect
+                    </span>
+                </button>
+            </div>
+
+            <div className="min-h-[150px] bg-[#0f0f0f] h-full">
+            </div>
+        </div>
+
+
+  </div>
+  )
+}
+
+export default cams
