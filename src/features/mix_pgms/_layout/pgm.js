6aad48b8db59c81dad874d55b9186d09103eebf1
kristianrybar <108004841+kristianrybar@users.noreply.github.com>
2023-04-07 20:06:47 +0200

    pgm component

---
+++ src/features/mix_pgms/_layout/pgm.js
@@ -0,0 +1,22 @@
+const pgm = () => {
+  return (
+    <div className="flex flex-col h-full">
+
+        <div className="bg-tranparent font-medium">
+            <button className="hover:bg-[#858585] hover:text-[#1a1a1a] text-center p-3">
+                PGM 1
+            </button>
+
+            <button className="hover:bg-[#858585] hover:text-[#1a1a1a] text-center p-3">
+                PGM 2
+            </button>
+        </div>
+
+        <div className="bg-[#0f0f0f] h-full">
+        </div>
+
+    </div>
+  )
+}
+
+export default pgm
