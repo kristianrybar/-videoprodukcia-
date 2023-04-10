7f2bb2d960584dfcf7f0eab2c1f735a83667f3e7
kristianrybar <108004841+kristianrybar@users.noreply.github.com>
2023-04-10 23:02:42 +0200

    fix

--- src/features/mix_layers/_layout/Layers.js
+++ src/features/mix_layers/_layout/Layers.js
@@ -12,10 +12,10 @@ const Layers = () => {
       </div>
 
       <Layer title='LAYER 1' />
-      <Layer title='LAYER 1' />
-      <Layer title='LAYER 1' />
-      <Layer title='LAYER 1' />
-      <Layer title='LAYER 1' />
+      <Layer title='LAYER 2' />
+      <Layer title='LAYER 3' />
+      <Layer title='LAYER 4' />
+      <Layer title='LAYER 5' />
 
       <div className="flex items-center text-[14px] mt-[3px]">
 
