ba9079d4eaea94fa0835f24d9723cb0512548981
kristianrybar <108004841+kristianrybar@users.noreply.github.com>
2023-04-13 00:18:30 +0200

    editing structure + added module.scss

--- src/features/app/mixergui/MixerGui.js
+++ src/features/app/mixergui/MixerGui.js
@@ -3,36 +3,18 @@ import Preview from '../../mix_preview/_layout/Preview'
 import MixModes from '../../mix_modes/_layout/MixModes'
 import Pgm from '../../mix_pgms/_layout/Pgm.js'
 import Cameras from '../../mix_cams/_layout/Cameras'
+import css from './MixerGui.module.scss'
 
-const mixergui = () => {
+const MixerGui = () => {
   return (
-    <div className="relative top-[70px] h-[100vh] bg-[#303030] text-[#a6a6a6] px-1">
-      <div className="grid grid-cols-12">
-
-        <div className='col-span-2 max-h-[300px]'>
+    <div className={`${css.wrapper} ${css.basicGrid}`}> 
           <Layers />  
-        </div>
-
-        <div className='col-span-3 max-h-[300px]'>
           <Preview />
-        </div>
-
-        <div className='col-span-2 max-h-[300px]'>
           <MixModes />  
-        </div>
-
-        <div className='col-span-3 max-h-[300px]'>
           <Pgm />
-        </div>
-
-        <div className='col-span-2'>
-          <Cameras />
-        </div>
-        
-      </div>
-
+          <Cameras />    
     </div>
   )
 }
 
-export default mixergui
+export default MixerGui
